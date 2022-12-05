44fc1745ab39e4e1aae39a3a4ba09ec7aa931d45
Artem Archenkov <r203mail@gmail.com>
2022-12-05 10:19:44 +0400

    v0.1.0

---
+++ server.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody({ json: true }));
+
+const messages = [
+  {
+    "id": 1,
+    "userId": "5f2d9da0-f624-4309-a598-8ba35d6c4bb6",
+    "content": "Какая сейчас погода за окном?"
+  },
+  {
+    "id": 2,
+    "userId": "5f2d9da0-f624-4309-a598-8ba35d6c4bb8",
+    "content": "К сожалению, я не знаю ответа на этот вопрос"
+  },
+];
+let nextId = 3;
+
+const router = new Router();
+
+router.get('/messages', async (ctx, next) => {
+  const from = Number(ctx.request.query.from)
+  if (ctx.request.query.from === 0) {
+    ctx.response.body = messages;
+    return;
+  }
+
+  const fromIndex = messages.findIndex(o => o.id === from);
+  if (fromIndex === -1) {
+    ctx.response.body = messages;
+    return;
+  }
+
+  ctx.response.body = messages.slice(fromIndex + 1);
+});
+
+router.post('/messages', async (ctx, next) => {
+  messages.push({ ...ctx.request.body, id: nextId++ });
+  ctx.response.status = 204;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 7777;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('server started'));
